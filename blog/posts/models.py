fffdac4c123549c593b79f75789e0637fa2a43b3
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-02-11 15:27:31 +0200

    create a model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Posts(models.Model):
+    title=models.CharField(max_length=50)
+    text=models.CharField(max_length=100)
+    created=models.DateTimeField(default=datetime.now,blank=True)
